f9d474ca7e8088e18f1610f38493e75de715caae
arthur <funkyhomercaat@gmail.com>
2025-10-02 08:42:12 -0300

    ajuste no getCurrentTimestamp para disparo com horario de funcionamento

--- services/getCurrentTimestamp.js
+++ services/getCurrentTimestamp.js
@@ -3,7 +3,16 @@ const getCurrentTimestamp = () => {
 };
 
 const parseLocalDateTime = (dateTimeString) => {
-  return new Date(dateTimeString).getTime();
+  // Parse da string de data/hora no formato brasileiro (DD/MM/YYYY HH:mm:ss)
+  // e converte para timestamp considerando timezone local
+  const [datePart, timePart] = dateTimeString.split(' ');
+  const [day, month, year] = datePart.split('/');
+  const [hour, minute, second] = timePart.split(':');
+  
+  // Cria a data no timezone local
+  const localDate = new Date(year, month - 1, day, hour, minute, second || 0);
+  
+  return localDate.getTime();
 };
   
 module.exports = { getCurrentTimestamp, parseLocalDateTime };
